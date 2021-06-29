0254a6c80f0237ea51d013ec78e5fca93878abf1
Sergey Zhuravlev <sergey.a.zhuravlev@gmail.com>
2021-06-30 00:04:47 +0300

    changed: trying to find libpci in the system, build own copy if not found; use PkgConfig space for libpci ;

--- src/hello.c
+++ src/hello.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <pci/pci.h>
-//#include <lib.h>
+#include <lib.h>
+
+struct pci_access pci_acc;
 
 int main(int argc, char** argv)
 {
-//    int v = function();
-//    printf("Hello! %d\n", v);
+    int v = function();
+    printf("Hello! %d\n", v);
 
     return 0;
 }
